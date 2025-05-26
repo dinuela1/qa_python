06b1b86588aa76897d6255c0d68f63855f399a11
Dina Kodak <2dina.kodak@gmail.com>
2025-05-26 22:58:23 +0300

    added 10 tests

--- tests.py
+++ tests.py
@@ -2,9 +2,6 @@ import pytest as pytest
 
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-
 
 class TestBooksCollector:
 
